9988c5093fbb514cef778cdd5bd57952633c0fe0
Lauri <lauri.sakari.makinen@gmail.com>
2011-11-12 20:02:24 +0200

    Working on the enemies

--- main.lua
+++ main.lua
@@ -1,26 +1,37 @@
+message = "test";
+
 playerX = 250;
 playerY = 250;
 playerSpeed = 7;
+playerTimer = 10;
 
-BULLET_MAX_COUNT = 100;
+BULLET_MAX_COUNT = 500;
 bulletAlive = {};
 bulletX = {};
 bulletY = {};
 bulletSpeedX = {}
 bulletSpeedY = {}
-message = "test";
+
+ENEMY_MAX_COUNT = 250;
+enemyAlive = {}
+enemyType = {}
+enemyX = {}
+enemyY = {}
+
+enemySpawnerBaseTime = 100;
+enemySpawnerTime = 100;
+enemySpawnerTimeRate = 1;
 
 function love.load()
     img_player = love.graphics.newImage("player.png");
     img_enemy1 = love.graphics.newImage("enemy1.png");
 
     img_bullet = love.graphics.newImage("bullet.png");
-
 end
 
 function love.update(dt)
     --framelimit
-    if dt < 1/30 then
+    if dt < 1/60 then
         love.timer.sleep(1000 * (1/30 - dt))
     end
 
@@ -42,18 +53,53 @@ function love.update(dt)
     end
 
     if love.keyboard.isDown(" ") then
-        createBullet(playerX + 10,playerY - 15,0,-10);
+        playerTimer = playerTimer - 1;
+        if playerTimer < 0 then
+            createBullet(playerX + 10,playerY - 15,0,-7);
+            playerTimer = 5;
+        end
     end
 
     --update game
 
+    enemySpawner();
     updateBullets();
+    updateEnemies();
 end
 
 function love.draw()
-    love.graphics.draw(img_player,playerX,playerY);
-    renderBullets();
     love.graphics.print(message,0,0);
+
+    renderBullets();
+    renderEnemies();
+    love.graphics.draw(img_player,playerX,playerY);
+end
+
+function updateEnemies()
+    speed = 0;
+
+    for i=1,ENEMY_MAX_COUNT do
+        if enemyAlive[i] == true then
+            if enemyType[i] == 1 then
+                speed = 5;
+            end
+
+            enemyY[i] = enemyY[i] + speed;
+
+            if enemyY[i] > 700 then
+                enemyAlive[i] = false;
+            end
+        end
+    end
+end
+
+function enemySpawner()
+    enemySpawnerTime = enemySpawnerTime - enemySpawnerTimeRate;
+
+    if enemySpawnerTime < 0 then
+        enemySpawnerBaseTime = 100;
+        createEnemy(math.random(0,800),-10,1);
+    end
 end
 
 function updateBullets()
@@ -69,8 +115,27 @@ function updateBullets()
             if bulletY[i] > 700 then --TODO: add the real end of the screen value that is correct
                 bulletAlive[i] = false;
             end
+
+            --Collision on the ENEMIES THAT ARE EVIL AND BAD
+            for q=1,ENEMY_MAX_COUNT do
+                if enemyAlive[q] == true then
+                    if pointCollisionCheck(bulletX[i],bulletY[i],enemyX[q],enemyY[q],20,20) == true then 
+                        enemyAlive[q] = false;
+                    end
+                end
+            end
         end
     end
+end
+
+function pointCollisionCheck(x,y,x2,y2,sizeX,sizeY)
+    if x > x2  and y > y2 then
+        if x < (x2 + sizeX) and y < (y2 + sizeY) then
+            return true;
+        end
+    end
+
+    return false;
 end
 
 function createBullet(x,y,speedX,speedY)
@@ -95,3 +160,28 @@ function renderBullets()
         end
     end
 end
+
+function renderEnemies() 
+    for i=1,ENEMY_MAX_COUNT do
+        if enemyAlive[i] == true then
+            if enemyType[i] == 1 then
+                love.graphics.draw(img_enemy1,enemyX[i],enemyY[i]);
+            end
+        end
+    end
+end
+
+function createEnemy(x,y,type_t)
+    for i=1,ENEMY_MAX_COUNT do
+        if enemyAlive[i] ~= true then 
+            enemyAlive[i] = true;
+            enemyX[i] = x;
+            enemyY[i] = y;
+            enemyType[i] = type_t;
+            do return end
+        end
+    end
+    return 0;
+end
+
+
