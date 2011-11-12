1617f3ad7d998424f271d51f8d737b54190325a8
Lauri <lauri.sakari.makinen@gmail.com>
2011-11-12 15:02:30 +0200

    Getting the basic game structure up and running

--- main.lua
+++ main.lua
@@ -1,2 +1,34 @@
 function love.load()
+    img_player = love.graphics.newImage("player.png");
+    img_enemy1 = love.graphics.newImage("enemy1.png");
+
+    img_bullet = love.graphics.newImage("bullet.png");
+
+    playerX = 250;
+    playerY = 250;
+    playerSpeed = 5;
+
+    BULLET_MAX_COUNT = 100;
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("right") then
+        playerX = playerX + playerSpeed;
+    end
+
+    if love.keyboard.isDown("left") then
+        playerX = playerX - playerSpeed;
+    end
+
+    if love.keyboard.isDown("up") then
+        playerY = playerY - playerSpeed;
+    end
+
+    if love.keyboard.isDown("down") then
+        playerY = playerY + playerSpeed;
+    end
+end
+
+function love.draw()
+    love.graphics.draw(img_player,playerX,playerY);
 end
